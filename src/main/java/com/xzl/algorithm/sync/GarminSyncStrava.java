45a840910dd3233f60b52c2a34c4565350223f69
xuzilou <1215855088@qq.com>
2022-10-28 18:53:49 +0800

    q

--- src/main/java/com/xzl/algorithm/sync/GarminSyncStrava.java
+++ src/main/java/com/xzl/algorithm/sync/GarminSyncStrava.java
@@ -1,10 +1,14 @@
 package com.xzl.algorithm.sync;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.cron.CronUtil;
+import cn.hutool.cron.task.Task;
 import com.alibaba.fastjson.JSON;
 import com.xzl.algorithm.sync.garminBean.Activity;
 import com.xzl.algorithm.sync.stravaBean.StravaUpload;
 
 import java.io.File;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -19,15 +23,33 @@ public class GarminSyncStrava {
 
 
     public static void main(String[] args) {
-        List<Activity> garminList = garminServer.getGarminList();
-        System.out.println(JSON.toJSONString(garminList));
-        for (Activity activity : garminList) {
-            String filePath = garminServer.exportGpx(activity.getActivityId());
-            List<StravaUpload> upload = stravaServer.upload(new File(filePath));
-            System.out.println(JSON.toJSONString(upload));
-            for (StravaUpload stravaUpload : upload) {
-                System.out.println(stravaServer.uploadResults(stravaUpload.getId()));
+
+        System.out.println(DateUtil.tomorrow().getTime());
+        CronUtil.schedule("0 0/2 * * * ? *", new Task() {
+            @Override
+            public void execute() {
+                Date LastTime = new Date();
+                List<Activity> garminList = garminServer.getGarminList();
+                System.out.println(JSON.toJSONString(garminList));
+                for (Activity activity : garminList) {
+                    Date startTimeLocal = activity.getStartTimeLocal();
+                    if (DateUtil.compare(LastTime, startTimeLocal) > 0) {
+                        continue;
+                    }
+                    String filePath = garminServer.exportGpx(activity.getActivityId());
+                    List<StravaUpload> upload = stravaServer.upload(new File(filePath));
+                    System.out.println(JSON.toJSONString(upload));
+                    for (StravaUpload stravaUpload : upload) {
+                        System.out.println(stravaServer.uploadResults(stravaUpload.getId()));
+                    }
+                    LastTime = new Date();
+                }
             }
-        }
+        });
+
+        CronUtil.setMatchSecond(true);
+        CronUtil.start();
+
+
     }
 }
